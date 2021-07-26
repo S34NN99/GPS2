f49284d04e2ee587e938cd47523bc65e6d10f579
S34NN99 <seanz995.my@gmail.com>
2021-07-26 19:57:37 +0800

    Fix bugs

--- GPS2_FireSquad/Assets/Scripts/Interfaces.cs
+++ GPS2_FireSquad/Assets/Scripts/Interfaces.cs
@@ -1,19 +1,17 @@
 using System;
 using UnityEngine;
 
-public interface IAnimation 
-{
-    void Walking(bool isWalking);
-    void UsingMainSkill(bool isUsingSkill);
-    void UsingSecondarySkill(bool isUsingSkill);
-    void UsingUniqueSkill(string skillName, bool isUsingSkill);
-}
-
 public interface IPlayer
 {
     void Stun(PlayerMovement playerMovement);
     void UnStun(PlayerMovement playerMovement);
     void SpawnFire(PlayerMovement playerMovement, GameObject firePrefab);
+
+    //ANIMATIONS
+    void Walking(bool isWalking);
+    void UsingMainSkill(bool isUsingSkill);
+    void UsingSecondarySkill(bool isUsingSkill);
+    void UsingUniqueSkill(string skillName, bool isUsingSkill);
 }
 
 public interface IFmod
